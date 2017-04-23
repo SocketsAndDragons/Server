03d0ff7ddba8aca53c279f3de8dca5c3b3a81ca4
bonattt <bonattt@rose-hulman.edu>
2017-04-23 16:15:48 -0400

    server.py runs mock server in background, having shell execute commands every 5 seconds.

--- Server.py
+++ Server.py
@@ -1,7 +1,9 @@
-import site
-site.main()
+import threading
+import time
 
-from Server.sagServer import shell
+
+from Server.sagServer.shell import Shell
+from Server.sagServer.shell import HelpCommand
 
 PORT = 2466
 
@@ -9,14 +11,28 @@ def main():
     # TODO open welcome socket
     port = PORT
     print("welcome to sockets and dragons! server is listening on port", port)
-    myShell = createNewShell()
-    myShell.run()
+    shell = createNewShell()
+    server_thread = start_server(shell)
+    shell.run()
+    print("killing server thread")
+    server_thread.isAlive = False
+    server_thread.join(5000)
+    print("have a nice day!")
 
+def start_server(shell):
+    t = threading.Thread(target=run_server, args=(shell,))
+    t.start()
+    return t
+
+def run_server(shell):
+    while True:
+        time.sleep(5)
+        shell.execute('help', ['help'])
 
 def createNewShell():
-    myShell = shell.Shell()
-    myShell.gmCmd['help'] = shell.HelpCommand()
-    return myShell
+    shell = Shell()
+    shell.gmCmd['help'] = HelpCommand()
+    return shell
 
 
 if __name__ == "__main__":
--- sagServer/shell.py
+++ sagServer/shell.py
@@ -1,9 +1,14 @@
+
+import sys
 from Server.sagServer import map
 from Server.sagServer import room
 from Server.sagServer import playerCommands
 from Server.sagServer import gmCommands
 
 class Shell:
+
+    CMD_PROMPT = 'SaG?> '
+    DEBUG = True
 
     def __init__(self, width=10, height=10, startingActions=0):
 
@@ -16,22 +21,40 @@ class Shell:
 
 
     def run(self):
+        self.display()
         while True:
-            gmIn = input("SaG?> ")
+            gmIn = input()
             if gmIn == 'quit' or gmIn == 'exit':
                 gmIn = input('are you sure you would like to exit? (y/N)')
                 if gmIn.startswith('y'):
                     break
 
             args = gmIn.split()
-            cmdName = args[0]
+            self.execute(args[0], args)
+
+    def execute(self, cmdName, args):
+        try:
             if cmdName in self.gmCmd:
                 cmd = self.gmCmd[cmdName]
-                cmd.execute(self, args[1:])
-
+                success = cmd.execute(self, args)
             else:
-                print("command not recognized")
+                self.display("command not recognized")
+        except Exception as e:
+            self.display("an error occurred executing the command", cmdName,"with the arguments", args)
+            raise e
 
+    def display(self, *args, lastMsg=True, debug=False):
+        # do not print debug messages outside debugt mode
+        if (debug and not self.DEBUG): return
+
+        msg = ''
+        for arg in args:
+            msg += str(arg)
+            msg += ' '
+        print(msg)
+        if lastMsg:
+            sys.stdout.write(self.CMD_PROMPT)
+            sys.stdout.flush()
 
 
 class HelpCommand:
@@ -40,22 +63,24 @@ class HelpCommand:
         self.short_help_msg = "this command prints all availible GM commands"
 
     def execute(self, shell, args):
-        if len(args) == 0:
+        if len(args) == 1:
             self.list_all_cmds(shell)
 
-        elif len(args) == 1:
+        elif len(args) == 2:
             self.help_for_cmd(shell, args)
 
         else:
-            print("incorrect args. try 'help' or 'helf <command name>'")
+            shell.display("incorrect args. try 'help' or 'helf <command name>'")
+            return False
+        return True
 
-    def help(self):
-        print(self.short_help_msg)
+    def help(self, shell):
+        shell.display(self.short_help_msg)
 
     def help_for_cmd(self, shell, args):
         cmdName = args[0]
         cmd = shell.gmCmd[cmdName]
-        cmd.help()
+        cmd.help(shell)
 
     def list_all_cmds(self, shell):
         msg = 'the GM can use the following commands:\n'
@@ -64,4 +89,4 @@ class HelpCommand:
             msg += shell.gmCmd[cmdName].short_help_msg
             msg += '\n'
 
-        print(msg)
+        shell.display(msg)
