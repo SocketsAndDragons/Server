412b48070eb6c1b640e1d29cdb47bfcae6d6b799
bonattt <bonattt@rose-hulman.edu>
2017-04-23 14:12:23 -0400

    map initializes with rooms of correct dimensions

--- sagServer/map.py
+++ sagServer/map.py
@@ -1,6 +1,16 @@
-
+from Server.sagServer import room
 
 class Map():
 
     def __init__(self, width, height):
-        pass
+        self.rooms = self.__initRooms__(width, height)
+
+    def __initRooms__(self, width, height):
+        rooms = []
+        for i in range(width):
+            row = []
+            for j in range(height):
+                row.append(room.Room())
+            rooms.append(row)
+
+        return rooms
--- test/mapTest.py
+++ test/mapTest.py
@@ -10,8 +10,27 @@ class MapTest(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def testOne(self):
-        myMap = map.Map(1, 2)
+    def testInitRoomsWidth(self):
+        myMap = map.Map(5, 1)
+        myMap.rooms[4][0]
+        # as long as an exception isn't thrown, you're good
+
+    def testInitRoomsWidthNotTooBig(self):
+        myMap = map.Map(4, 2)
+        with self.assertRaises(IndexError):
+            myMap.rooms[4][0]
+        # as long as an exception isn't thrown, you're good
+
+    def testInitRoomsHeight(self):
+        myMap = map.Map(3, 6)
+        myMap.rooms[0][5]
+        # as long as an exception isn't thrown, you're good
+
+    def testInitRoomsHeightNotToBig(self):
+        myMap = map.Map(2, 4)
+        with self.assertRaises(IndexError):
+            myMap.rooms[0][4]
+        # as long as an exception isn't thrown, you're good
 
 
 if __name__ == "__main__":
